2f9a304dabf66a2b9120c949d77128e99d5f26f5
amdydesign <amdyns@gmail.com>
2026-01-16 14:44:27 +0100

    security: remove hardcoded secrets, del debug files, secure htaccess

--- api/config/social_credentials.php
+++ api/config/social_credentials.php
@@ -1,33 +1,31 @@
 <?php
 return [
     'youtube' => [
-        'api_key' => 'AIzaSyCli85Ks3BQTUxwNYCUVafMhkXDOBQOm8A',
-        // Channel ID can be hardcoded here OR stored in database via frontend
-        // We will store it in database for flexibility
+        'api_key' => $_ENV['SOCIAL_YOUTUBE_API_KEY'] ?? '',
     ],
     'tiktok' => [
-        'client_key' => 'sbawlp0dkocuzeoueq',
-        'client_secret' => 'hpA0SL7aA19rEhOrIvdSKmCcGl8xRQma',
-        'redirect_uri' => 'https://panel.malgorzatamordarska.pl/auth/callback/tiktok'
+        'client_key' => $_ENV['SOCIAL_TIKTOK_CLIENT_KEY'] ?? '',
+        'client_secret' => $_ENV['SOCIAL_TIKTOK_CLIENT_SECRET'] ?? '',
+        'redirect_uri' => $_ENV['SOCIAL_TIKTOK_REDIRECT_URI'] ?? ''
     ],
     'facebook' => [
-        'app_id' => '770922815351899',
-        'app_secret' => '4d87d1098cfe2a1eed87417f255200f0',
-        'redirect_uri' => 'https://panel.malgorzatamordarska.pl/auth/callback/facebook'
+        'app_id' => $_ENV['SOCIAL_FACEBOOK_APP_ID'] ?? '',
+        'app_secret' => $_ENV['SOCIAL_FACEBOOK_APP_SECRET'] ?? '',
+        'redirect_uri' => $_ENV['SOCIAL_FACEBOOK_REDIRECT_URI'] ?? ''
     ],
     'instagram' => [
-        'app_id' => 'YOUR_FACEBOOK_APP_ID',
-        'app_secret' => 'YOUR_FACEBOOK_APP_SECRET',
-        'redirect_uri' => 'https://panel.malgorzatamordarska.pl/auth/callback/instagram'
+        'app_id' => $_ENV['SOCIAL_INSTAGRAM_APP_ID'] ?? '',
+        'app_secret' => $_ENV['SOCIAL_INSTAGRAM_APP_SECRET'] ?? '',
+        'redirect_uri' => $_ENV['SOCIAL_INSTAGRAM_REDIRECT_URI'] ?? ''
     ],
     'rapidapi' => [
-        'key' => 'e895b43fd6mshad615eb9a25a5c0p13b340jsn697e00c6bcbe', // To be filled by user
-        'host' => 'instagram-looter2.p.rapidapi.com',
-        'username' => 'gosia_mordarska' // Target profile
+        'key' => $_ENV['SOCIAL_RAPIDAPI_KEY'] ?? '',
+        'host' => $_ENV['SOCIAL_RAPIDAPI_INSTAGRAM_HOST'] ?? '',
+        'username' => 'gosia_mordarska'
     ],
     'facebook_rapidapi' => [
-        'key' => 'e895b43fd6mshad615eb9a25a5c0p13b340jsn697e00c6bcbe',
-        'host' => 'facebook-scraper-api4.p.rapidapi.com',
+        'key' => $_ENV['SOCIAL_RAPIDAPI_KEY'] ?? '',
+        'host' => $_ENV['SOCIAL_RAPIDAPI_FACEBOOK_HOST'] ?? '',
         'url' => 'https://www.facebook.com/profile.php?id=61577001039304'
     ]
 ];
